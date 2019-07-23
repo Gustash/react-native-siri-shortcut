3d3cae9a5f9ae38d325633ff8440af25bae01fdf
Gustavo Parreira <gustavo.parreira@mypthub.net>
2019-07-23 10:32:44 +0100

    Brought back Brigding Header imports.

--- ios/RNSiriShortcuts-Bridging-Header.h
+++ ios/RNSiriShortcuts-Bridging-Header.h
@@ -3,10 +3,10 @@
 //
 
 
-// #import <React/RCTBridgeModule.h>
-// #import <React/RCTBridge.h>
-// #import <React/RCTConvert.h>
-// #import <React/RCTUtils.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTBridge.h>
+#import <React/RCTConvert.h>
+#import <React/RCTUtils.h>
 #import <React/RCTEventEmitter.h>
-// #import <React/RCTRootView.h>
+#import <React/RCTRootView.h>
 #import <React/RCTViewManager.h>
