784c855495f2bde6931e89cecd06da06fa3f7b0a
Gustavo Parreira <gustavo.parreira@mypthub.net>
2019-10-06 14:03:22 +0100

    Updated example project to use the React Native 0.61.2 and removed necessity for react-native-swift.

--- example/react-native.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  dependencies: {
-    "react-native-siri-shortcut": {
-      platforms: {
-        ios: null // disable autolinking as it has some issues for now
-      }
-    }
-  }
-};
