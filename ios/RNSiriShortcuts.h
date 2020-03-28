5eba9cf5578d42d5e3b32a862349b1714cf68c8f
Gustavo Parreira <gustavotcparreira@gmail.com>
2020-03-28 00:29:32 +0000

    Static Library support (#41)
    * Possible fix for needing use_frameworks
    
    * 1.7.0-alpha.0
    
    * Updated instructions in README
    
    * Added missing line.

---
+++ ios/RNSiriShortcuts.h
@@ -0,0 +1,13 @@
+//
+//  RNSiriShortcuts.h
+//  RNSiriShortcuts
+//
+//  Created by Gustavo Parreira on 27/03/2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#ifndef RNSiriShortcuts_h
+#define RNSiriShortcuts_h
+
+
+#endif /* RNSiriShortcuts_h */
